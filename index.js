df41750488a8a4f4376b0f272d5fde0982b15910
Júlia Lino <125381117+juliallino@users.noreply.github.com>
2023-10-24 10:47:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,32 +3,25 @@ const app = express(); //iniciando o express
 
 //criando a rota inicial
 app.get("/", function(req,res){
-    res.send("<h1>Bem vindo ao meu site!</h1>");
+    res.send("<h1>TRABALHO 2 NUVEM!</h1>");
 })
 
-//rota do cadastro de produtos
-app.get("/produtos", function(req,res){
-    res.send("<h1>Lista de Produtos!</h1>");
-})
-
-//rota com parametro 
-app.get("/consulta/:parametro", function(req,res){
-    //req --> dados enviados pelo cliente
-    //res --> resposta enviada pelo servidor de volta ao cliente
-    res.send("retorno consulta:" + req.params.parametro);
+//rota da equipe
+app.get("/equipe", function(req,res){
+    res.send("<h1>JULIA LINO ARAÚJO e ALAN REGIS!</h1>");
 })
 
 //rota com parametro opcional
-app.get("/cadastro/:nome?", function(req,res){
-    //req --> dados enviados pelo cliente
+app.get("/aluno/:nome?", function(req,res){
     var nome = req.params.nome;
     if (nome){
-        res.send("<h1>produto " + nome + " criado!</h1>");
-    }else{
-        res.send("produto criado!");
+        res.send("<h1>Aluno: " + nome);
+    }else {
+        res.send("<h1>Aluno não encontrado<h1>");
     }
     
 })
+
 
 app.listen(process.env.PORT ?? 3000,function(erro){  // cria a aplicação na porta 4000
     if (erro){
@@ -37,4 +30,3 @@ app.listen(process.env.PORT ?? 3000,function(erro){  // cria a aplicação na po
         console.log("Servidor Iniciado.");
     }
 })
-
